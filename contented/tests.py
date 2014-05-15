30e67af96a14998d5790720540c8a802d2c3f105
Ian George <ian@quiet.sh>
2014-05-15 10:52:25 +0100

    Fixing up the first lot of tests and trying to get travis ci working

--- contented/tests.py
+++ contented/tests.py
@@ -6,18 +6,19 @@ from app import Application
 class SettingsTests(unittest.TestCase):
     def test_global_settings(self):
         s = get_settings({})
-        self.assertEqual(s.DEBUG, False)
+        self.assertEqual(s.debug, False)
 
     def test_override_global(self):
-        s = get_settings({"DEBUG": True})
-        self.assertEqual(s.DEBUG, True)
+        s = get_settings({"debug": True})
+        self.assertEqual(s.debug, True)
 
 
 class AppTests(unittest.TestCase):
     def test_load_app(self):
         app = Application({})
         self.assertTrue(hasattr(app, "settings"))
-        self.assertTrue(hasattr(app, "jinja_env"))
+        self.assertTrue(hasattr(app, "content_map"))
+        self.assertTrue(hasattr(app, "request_processors"))
 
 if __name__ == "__main__":
     unittest.main()
